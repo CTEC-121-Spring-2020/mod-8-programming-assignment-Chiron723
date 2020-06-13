db291f898a222b3920b292b0546ea33d44f70342
Stephen Guild <s.guild2@students.clark.edu>
2020-06-12 22:57:56 -0700

    Class Assignment

--- Prob-1/Prob-1.py
+++ Prob-1/Prob-1.py
@@ -3,3 +3,41 @@
 #     Prob-1.py
 
 # Your code below
+class car:
+    def __init__(self, make, model, year):
+        self._make=make
+        self._model=model
+        self._year=year
+
+    def set_make(self,make):
+        self._make=make
+
+    def get_Make(self):
+        return self._make
+    
+    def set_model(self,model):
+        self._model=model
+        
+    def get_model(self):
+        return self._model
+
+    def set_year(self,year):
+        self._year=year
+    
+    def get_year(self):
+        return self._year
+    
+def TestCar():
+    autoLot=[]
+    autoLot.append(car("make\t", "model\t", "year"))
+    autoLot.append(car("Toyota\t","Corola",1984))
+    autoLot.append(car("Mercury","Sable\t", 1995))
+    autoLot.append(car("Datsun\t","Cherry\t", 1975))
+    autoLot.append(car("DMC\t","Delorian",1984))
+    autoLot.append(car("Volkswagon","Beetle",1970))
+    for i in range(len(autoLot)):
+        print(autoLot[i].get_Make(),"\t",autoLot[i].get_model(),"\t",autoLot[i].get_year())
+
+if __name__ == "__main__":
+    TestCar()
+
--- Prob-2/Prob-2.py
+++ Prob-2/Prob-2.py
@@ -3,3 +3,61 @@
 #     Prob-2.py
 
 # Your code below
+class Student:
+    """I chose to focus on name and ID number because in any 
+    school setting those are almost always the two most 
+    important details, the major and gpa are generally the 
+    details that you are searching for anyways"""
+    def __init__(self,name,IDNumber):
+        self._name=name
+        self._IDNumber=IDNumber
+        self._major=""
+        self._gpa=0.0
+        
+    def set_name(self,name):
+        self._name=name
+    
+    def get_name(self):
+        return self._name
+    
+    def set_IDNumber(self,IDNumber):
+        self._IDNumber=IDNumber
+    
+    def get_IDNumber(self):
+        return self._IDNumber
+
+    def set_major(self, major):
+        self._major=major
+    
+    def get_major(self):
+        return self._major
+    
+    def set_gpa(self,gpa):
+        self._gpa=gpa
+    
+    def get_gpa(self):
+        return self._gpa
+
+def TestClass():
+    records=[]
+    records.append(Student("Joe Bella\t",9933))
+    records.append(Student("Tucker Blank\t",3399))
+    records.append(Student("Gayle Ujifusa\t",1011))
+    records.append(Student("Edna Anker\t",9875))
+    records[0].set_major("Web Development")
+    records[1].set_major("Nursing")
+    records[2].set_major("Baking")
+    records[3].set_major("Medical Office")
+    records[0].set_gpa(3.8)
+    records[1].set_gpa(3.0)
+    records[2].set_gpa(2.8)
+    records[3].set_gpa(3.0)
+    
+    for i in records:
+        print(i.get_name())
+        print(i.get_IDNumber())
+        print(i.get_major())
+        print(i.get_gpa())
+        
+if __name__ == "__main__":
+    TestClass()
